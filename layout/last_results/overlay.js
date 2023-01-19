233b1a2d78ee4f3274fdc06bda48b82497cca6ab
TwilCynder <twilcynder@gmail.com>
2023-01-19 17:17:59 +0100

    last results unsuccessful attempts

--- layout/last_results/overlay.js
+++ layout/last_results/overlay.js
@@ -30,17 +30,64 @@ $(() => {
     add_set("SnooSnoo", 2, "Nacy's Bitch", 1, 4);
 
     let res = "";
+        /*
+        $("#R1").html('loading....');
 
-    $("#R1").html('loading....');
-
-    $.ajax({url: "https://cors-anywhere.herokuapp.com/https://api.start.gg/gql/alpha",
-        contentType: "application/json",type:'POST',
-        data: JSON.stringify({ query:`{
-            sayHello(name:"${res}")}`
-        }),
-        success: function(result) {
-            console.log(JSON.stringify(result));
-            $("#R1").html(JSON.stringify(result));
+        $.ajax({url: "https://localhost:3000/",
+           contentType: "application/json",type:'POST',
+           data: JSON.stringify({ query:`{
+              sayHello(name:"${res}")}`
+           }),
+           success: function(result) {
+              console.log(JSON.stringify(result));
+              $("#R1").html(JSON.stringify(result));
+           }
+        });
+        */
+    
+    const schema = `
+    query Query($slug: String, $setNum: Int) {
+        event(slug: $slug){
+          sets(page: 1, perPage: $setNum, sortType:RECENT, filters:{
+            state: 3
+          }){
+            nodes{
+              slots {
+                   standing{
+                         stats{
+                              score{
+                                  value
+                               }
+                         }
+                    }
+                    entrant {
+                         id
+                         name
+                    }            
+              }
+            }
+          }
         }
-    });
+      }
+    `
+      
+    fetch('https://api.start.gg/gql/alpha', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'authorization': 'Bearer d0206138a8ea04cf8e34f80ecc177663'
+        },
+        body: JSON.stringify({
+            query: schema,
+            variables: {
+                "slug": "tournament/stock-o-clock-23/event/1v1-ultimate",
+                "setNum": 5
+            },
+        }), 
+        mode: 'no-cors',
+    })
+        
+        
+    //$("#R1").html(res);
 })
