b7de8b36d882fc1aa3091d0712f1eb9326c0865b
TwilCynder <twilcynder@gmail.com>
2023-04-27 19:10:59 +0200

    tournament history

--- layout/last_results/overlay.js
+++ layout/last_results/overlay.js
@@ -69,7 +69,7 @@ $(() => {
             body: JSON.stringify({
                 'query': query,
                 'variables' : {
-                    "slug": "tournament/stock-o-clock-37/event/1v1-ultimate",
+                    "slug": "tournament/stock-o-clock-38/event/1v1-ultimate",
                     "setNum": 6
                 } 
             }),  
