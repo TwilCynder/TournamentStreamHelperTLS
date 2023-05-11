d03b67985cd52687341920e0e086e37a67e99c46
TwilCynder <twilcynder@gmail.com>
2023-05-11 17:33:46 +0200

    modernize recentsetsTLS

--- layout/recent_setsTLS/index.js
+++ layout/recent_setsTLS/index.js
@@ -1,4 +1,6 @@
-(($) => {
+update_delay = 4000;
+
+LoadEverything().then((triggerUpdate) => {
   let startingAnimation = gsap
     .timeline({ paused: true })
     .from($(".recent_sets"), { autoAlpha: 0 });
@@ -6,12 +8,9 @@
   var playersRecentSets = null;
   var players = null;
 
-  async function Start() {
+  Start = async (event) => {
     startingAnimation.restart();
-  }
-
-  var data = {};
-  var oldData = {};
+  };
 
   function generatePlayerHTML(player, id){
     if (player) {
@@ -28,9 +27,10 @@
     return "";
   }
 
-  async function Update() {
-    oldData = data;
-    data = await getData();
+  Update = async (event) => {
+    
+    let data = event.data;
+    let oldData = event.oldData;
 
     if (
       !oldData.score ||
@@ -38,30 +38,29 @@
         JSON.stringify(data.score.recent_sets)
     ) {
       playersRecentSets = data.score.recent_sets;
+      console.log(playersRecentSets);
     } else {
-      return;
+      return; //for performance ??
     }
 
-    players = "";
     recentSetsHtml = "";
+    players = "";
 
     if (
       playersRecentSets == null ||
       (playersRecentSets.state == "done" && playersRecentSets.sets.length == 0)
     ) {
-      recentSetsHtml += ``;
-      players += ``;
+      recentSetsHtml = `No sets found`;
     } else if (playersRecentSets.state != "done") {
       recentSetsHtml += `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`;
-      players = "";
     } else {
+
       let count = 0;
       let p1Victories = 0;
       let p2Victories = 0;
 
-      playersRecentSets.sets.forEach( (_set) => {
-        if (count < 5){
-
+      for (const _set of playersRecentSets.sets) {
+        if (count < 5 ){
           recentSetsHtml += `
             <div class="set_container">
               <div class="${_set.winner == 0 ? "set_winner" : "set_loser"}">
@@ -82,19 +81,19 @@
                       )}
                     </div>
                 </div>
-              <div class="set_data">
-                <div class="set_phase">
-                  ${_set.phase_id}
-                  ${_set.phase_name}
-                </div>
-                  <div class="set_round">
-                    ${_set.round}
+                <div class="set_data">
+                  <div class="set_phase">
+                    ${_set.phase_id}
+                    ${_set.phase_name}
+                  </div>
+                    <div class="set_round">
+                      ${_set.round}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="${_set.winner == 1 ? "set_winner" : "set_loser"}">
-                ${_set.score[1]}
-              </div>
+                <div class="${_set.winner == 1 ? "set_winner" : "set_loser"}">
+                  ${_set.score[1]}
+                </div>
             </div>
           `;
 
@@ -106,8 +105,7 @@
         } else {
           p2Victories++;
         }
-
-      })
+      }
 
       console.log(p1Victories, p2Victories);
       $("#set_count").html(`${p1Victories} - ${p2Victories}`);
@@ -124,59 +122,11 @@
       team = data.score.team["2"];
       if (team)
           players += generatePlayerHTML(team.player["1"], 2)
-
-      /*
-      playersRecentSets.sets.slice(0, 5).forEach((_set) => {
-        recentSetsHtml += `
-            <div class="set_container">
-              <div class="${_set.winner == 0 ? "set_winner" : "set_loser"}">
-                ${_set.score[0]}
-              </div>
-              <div class="set_info">
-                <div class="set_title">
-                    ${_set.online ? `<div class="wifi_icon"></div>` : ""}
-                    ${_set.tournament}
-                    <div class="set_date">
-                      ${new Date(_set.timestamp * 1000).toLocaleDateString(
-                        "en-US",
-                        {
-                          month: "short",
-                          day: "2-digit",
-                          year: "numeric",
-                        }
-                      )}
-                    </div>
-                </div>
-              <div class="set_data">
-                <div class="set_phase">
-                  ${_set.phase_id}
-                  ${_set.phase_name}
-                </div>
-                  <div class="set_round">
-                    ${_set.round}
-                  </div>
-                </div>
-              </div>
-              <div class="${_set.winner == 1 ? "set_winner" : "set_loser"}">
-                ${_set.score[1]}
-              </div>
-            </div>
-          `;
-      });
-      */
     }
 
     SetInnerHtml($(`.recent_sets_players`), players);
     SetInnerHtml($(`.recent_sets_content`), recentSetsHtml);
-  }
+  };
 
-  // Using update here to set images as soon as possible
-  // so that on window.load they are already preloaded
-  Update();
-  $(window).on("load", () => {
-    $("body").fadeTo(0, 1, async () => {
-      Start();
-      setInterval(Update, 4000);
-    });
-  });
-})(jQuery);
+  triggerUpdate();
+});
